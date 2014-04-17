b110a6286a4601dd179bea3bd2be261d00cdd7cd
kewle003 <markkewley@hotmail.com>
2014-04-17 14:58:48 -0500

    Added in threading to ProxyServer

---
+++ src/proxy/ProxyServer.java
@@ -0,0 +1,56 @@
+package proxy;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ProxyServer {
+    
+    // Data Members: 
+    int proxyPort; // Proxy’s port 
+    ServerSocket proxySock; // The Proxy Server will listen on this socket 
+    
+    // serverSock represents the Proxy’s connection with a HTTP server 
+    Socket serverSock; 
+    
+    // Constructor 
+    public ProxyServer (String configfilePath, int portNo) { 
+        // Read the config file, and populate appropriate data structures. 
+        // Create Server socket on the proxyPort and wait for the clients to 
+        // connect. 
+        //ConfigFile cf = new ConfigFile(configfilePath);
+        proxyPort = portNo;
+    
+        try {
+            proxySock = new ServerSocket(proxyPort);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+        System.out.println("Server Up");
+        System.out.println("Server Details: " + proxySock.toString());
+        while (true) { 
+            try {
+                new ProxyThread(proxySock.accept()).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } // end of while 
+    } 
+    
+    
+    // main method 
+    public static void main(String args[]) throws IOException { 
+        // Read the config file name and the Proxy Port. 
+        // Do error checking. If no config file is specified, or if no Port is specified 
+        // then exit. 
+        if (args.length != 1) {
+            System.out.println(args[0]);
+            throw new IOException("Usage: ProxyServer [0-9999]");
+        }
+        
+        int portNo = Integer.parseInt(args[0]);
+        
+        new ProxyServer("", portNo);
+    } 
+
+}
